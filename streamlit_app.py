5748355356a3f514c6bb05e8d94fee1bd27f717d
jackhozz <jackson.zerpa.13@gmail.com>
2025-05-05 13:17:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,17 @@ import pandas as pd
 import streamlit as st
 
 # Show app title and description.
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
 st.title("🎫 Support tickets")
 st.write(
